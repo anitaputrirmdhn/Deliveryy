72569beb9a2cef540f9663dcad67b47a76e32e3a
anitaputri <anitaaramadhani@gmail.com>
2023-01-05 20:35:37 +0700

    penambahan login

--- app/src/main/java/com/riza/delivery/loginActivity.java
+++ app/src/main/java/com/riza/delivery/loginActivity.java
@@ -1,14 +1,14 @@
-package com.anita.delivery;
+package com.riza.delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login2);
     }
 }
